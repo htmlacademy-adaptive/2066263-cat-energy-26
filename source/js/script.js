4e8f0c2c9a0c6024e35692075a65a80001bd7134
Sveta_Burakova <svetlaburs@gmail.com>
2022-08-29 01:35:44 +0300

    Добавляет спрайт

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,22 @@
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toggle');
+// let navMain = document.querySelector('.main-nav');
+// let navToggle = document.querySelector('.main-nav__toggle');
 
-navMain.classList.remove('main-nav--nojs');
+// navMain.classList.remove('main-nav--nojs');
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
+// navToggle.addEventListener('click', function () {
+//   if (navMain.classList.contains('main-nav--closed')) {
+//     navMain.classList.remove('main-nav--closed');
+//     navMain.classList.add('main-nav--opened');
+//   } else {
+//     navMain.classList.add('main-nav--closed');
+//     navMain.classList.remove('main-nav--opened');
+//   }
+// });
+
+const button = document.querySelector('.main-nav__button'); // находим кнопку
+const nav = document.querySelector('.main-nav'); // находим навигацию
+const closedClass = 'main-nav--closed'; // класс, который будем добавлять или удалять с навигации
+
+button.addEventListener('click', () => { // слушаем клике на кнопке
+  nav.classList.toggle(closedClass); // Как только на кнопке произойдёт клик у навигации смениться класс main-nav--closed
 });
